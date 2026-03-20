2e0ffcbf68f89d364eb71c162ec7d0c243ae436d
Anurag Singh <anuragchauhan299@gmail.com>
2026-03-20 21:11:03 +0530

    added missing feature

--- src/main/java/movie/service/bookmyshow/controllers/ShowController.java
+++ src/main/java/movie/service/bookmyshow/controllers/ShowController.java
@@ -3,6 +3,7 @@ package movie.service.bookmyshow.controllers;
 import movie.service.bookmyshow.models.Theatre;
 import movie.service.bookmyshow.services.ShowService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/show")
 public class ShowController {
 
     private final ShowService showService;
--- src/main/java/movie/service/bookmyshow/controllers/TicketController.java
+++ src/main/java/movie/service/bookmyshow/controllers/TicketController.java
@@ -14,14 +14,12 @@ import movie.service.bookmyshow.services.PaymentService;
 import movie.service.bookmyshow.services.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/ticket")
 public class TicketController {
 
     private final TicketService ticketService;
